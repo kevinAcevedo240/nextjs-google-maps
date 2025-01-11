96d86bf3e6de578f0e87dc2a2843c00fa3e119da
Kevin Julian Acevedo Salazar <keju.acevedosa24@gmail.com>
2025-01-11 15:06:16 -0500

    Update: toast error on current location catch

--- src/app/[client]/_components/current-location.tsx
+++ src/app/[client]/_components/current-location.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Marker } from "@/types";
 import { LocateFixed } from "lucide-react";
 import React from "react";
+import { toast } from "sonner";
 
 interface CurrentLocationProps {
     onLocationUpdate: (marker: Marker) => void;
@@ -14,58 +15,71 @@ const CurrentLocation: React.FC<CurrentLocationProps> = ({ onLocationUpdate }) =
           timeout: 10000,
           maximumAge: 0,
         };
-    
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-            (position) => {
-              const { latitude, longitude } = position.coords;
-    
-              const geocoder = new google.maps.Geocoder();
-              geocoder.geocode(
-                {
-                  location: { lat: latitude, lng: longitude },
-                },
-                (results, status) => {
-                  if (status === "OK" && results && results[0]) {
-                    onLocationUpdate({
-                      name: "",
-                      latitude: latitude,
-                      longitude: longitude,
-                      address: results[0].formatted_address,
-                    });
-                  } else {
-                    console.error("Geocode error: ", status);
-                    onLocationUpdate({
-                      name: "",
-                      latitude: latitude,
-                      longitude: longitude,
-                      address: "",
-                    });
-                  }
-                }
-              );
-            },
-            (error) => {
-              console.error("Geolocation error: ", error.message);
-              switch (error.code) {
-                case error.PERMISSION_DENIED:
-                  console.log("Permiso denegado por el usuario.");
-                  break;
-                case error.POSITION_UNAVAILABLE:
-                  console.log("Información de ubicación no disponible.");
-                  break;
-                case error.TIMEOUT:
-                  console.log("Tiempo de espera agotado para obtener la ubicación.");
-                  break;
-                default:
-                  console.log("Error desconocido al obtener la ubicación.");
-              }
-            },
-            options
-          );
-        } else {
+
+        if (!navigator.geolocation) {
           console.error("Geolocalización no soportada por el navegador.");
+          return;
         }
+    
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+  
+            const geocoder = new google.maps.Geocoder();
+            geocoder.geocode(
+              {
+                location: { lat: latitude, lng: longitude },
+              },
+              (results, status) => {
+                if (status === "OK" && results && results[0]) {
+                  onLocationUpdate({
+                    name: "",
+                    latitude: latitude,
+                    longitude: longitude,
+                    address: results[0].formatted_address,
+                  });
+                } else {
+                  console.error("Geocode error: ", status);
+                  onLocationUpdate({
+                    name: "",
+                    latitude: latitude,
+                    longitude: longitude,
+                    address: "",
+                  });
+                }
+              }
+            );
+          },
+          (error) => {
+            console.error("Geolocation error: ", error.message);
+            switch (error.code) {
+              case error.PERMISSION_DENIED:
+              toast.error("Permiso denegado por el usuario.", {
+                className:
+                "text-lg border border-primary shadow-cartoon-small-xs dark:shadow-cartoon-small-dark dark:border-black",
+              });
+              break;
+              case error.POSITION_UNAVAILABLE:
+              toast.error("Información de ubicación no disponible.", {
+                className:
+                "text-lg border border-primary shadow-cartoon-small-xs dark:shadow-cartoon-small-dark dark:border-black",
+              });
+              break;
+              case error.TIMEOUT:
+              toast.error("Tiempo de espera agotado para obtener la ubicación.", {
+                className:
+                "text-lg border border-primary shadow-cartoon-small-xs dark:shadow-cartoon-small-dark dark:border-black",
+              });
+              break;
+              default:
+              toast.error("Error desconocido al obtener la ubicación.", {
+                className:
+                "text-lg border border-primary shadow-cartoon-small-xs dark:shadow-cartoon-small-dark dark:border-black",
+              });
+            }
+          },
+          options
+        );
       };
   return (
     <Button
