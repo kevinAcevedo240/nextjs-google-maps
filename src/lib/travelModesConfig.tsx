8f96eca371bf7be0c6c4f2b9c2c7e615b111eb35
Kevin Julian Acevedo Salazar <keju.acevedosa24@gmail.com>
2025-01-11 03:36:55 -0500

    update: size icon travel mode test

--- src/lib/travelModesConfig.tsx
+++ src/lib/travelModesConfig.tsx
@@ -4,21 +4,21 @@ export const travelModes = [
        {
          value: "BICYCLING",
          label: "Bicicleta",
-         icon: <BikeIcon className="size-3 sm:size-4" />,
+         icon: <BikeIcon className="size-4" />,
        },
        {
          value: "WALKING",
          label: "Caminar",
-         icon: <FootprintsIcon className="size-3 sm:size-4" />,
+         icon: <FootprintsIcon className="size-4" />,
        },
        {
          value: "DRIVING",
          label: "Carro",
-         icon: <CarFront className="size-3 sm:size-4" />,
+         icon: <CarFront className="size-4" />,
        },
        {
          value: "TRANSIT",
          label: "Bus",
-         icon: <Bus className="size-3 sm:size-4" />,
+         icon: <Bus className="size-4" />,
        },
      ];
